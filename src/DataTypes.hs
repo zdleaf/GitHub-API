6102ba047515ac11f3fbb872a11e2b45cfbc6546
Z Leaf <zach@zincldn.co.uk>
2019-11-26 22:37:26 +0000

    minor comment changes

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -12,16 +12,12 @@ import Control.Monad (mzero)
 -- a data type to handle API response for public repos
 data Reporesponse = Reporesponse
                {
-                  id :: Integer, -- github api id
+                  id :: Integer, -- github repo id
                   languages_url :: String
                }
                deriving (Eq, Show, Generic)
 
 instance FromJSON Reporesponse
-   {-
-   parseJSON (Object v) =
-      Reporesponse   <$> v .: "id"
-                     <*> v .: "languages_url"
-   parseJSON _ = mzero
-   -}
+-- we do not need to specify details since we're deriving Generic
+
 
