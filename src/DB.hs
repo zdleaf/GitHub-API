3eccb9622cd521f8afe3855517f3cec6b37f917d
Z Leaf <zach@zincldn.co.uk>
2019-11-27 22:49:51 +0000

    tidy up code

--- src/DB.hs
+++ src/DB.hs
@@ -37,72 +37,32 @@ connectDB connection = do
 test_response = (Reporesponse 1 "test" "test")
 
 --addRepo :: Connection -> Reporesponse -> IO ()
-addRepo connection (Left str) = return ()
-addRepo connection (Right repoObject) = do
+addRepo connection (Left err) = return ()
+addRepo connection (Right repoResponse) = do
     run connection "INSERT INTO Reporesponses (gitID, languageURL, contributorsURL )\
                         \VALUES (?, ?, ?)"
                         [
-                        (toSql (D.id repoObject)),
-                        (toSql (languages_url repoObject)),
-                        (toSql (contributors_url repoObject))
+                        (toSql (D.id repoResponse)),
+                        (toSql (languages_url repoResponse)),
+                        (toSql (contributors_url repoResponse))
                         ]
     commit connection
     return ()
 
--- extract response either Left/Right in reponse list
+-- extract response either Left/Right in response list
 extractResp (Left err) = []
 extractResp (Right list) = list
 
+addRepoMany :: IConnection t => t -> [Either a Reporesponse] -> IO b
+addRepoMany db (x:xs) = do
+    addRepo db x
+    addRepoMany db xs
 
+{-
 --addRepoMany db responseList = fmap (addRepo db) responseList
+
 extractRepo (Left err) = do
     print $ "extractRepo Left: " ++ err
     return Reporesponse {}
 extractRepo (Right repo) = repo
-
-addRepoMany :: IConnection t => t -> [Either a Reporesponse] -> IO b
-addRepoMany db (x:xs) = do
-    addRepo db $ x
-    addRepoMany db xs
-
-{-
-extractResponseList :: Either [Char] [Either String D.Reporesponse] -> Either (IO (), [Either String D.Reporesponse])      
-extractResponseList response =
-    case response of
-    Left str -> print $ "error Left in extractResponseList: " ++ str
-    Right list -> return list
-
-addRepoMany [] db = print $ "empty list"
-addRepoMany (x:xs) db = do
-    case x
-    
-    if extractResp x == 
-    | x == Left str = do
-        print $ "error Left in addRepoMany: " ++ str
-        addRepoMany xs
-    | x == Right repo = do
-        addRepo db repo
-        addRepoMany xs
-
-
-
-addRepoMany [] db = print $ "empty list"
-addRepoMany (x:xs) db = if extractResp x == 
-    | x == Left str = do
-        print $ "error Left in addRepoMany: " ++ str
-        addRepoMany xs
-    | x == Right repo = do
-        addRepo db repo
-        addRepoMany xs
-
-        
-addRepoMany [] db = []
-addRepoMany (x:xs) db =
-    case x of
-        Left str -> do
-            print $ "error Left in addRepoMany: " ++ str
-            addRepoMany xs db
-        Right repo -> do
-            addRepo db repo
-            addRepoMany xs db
-            -}
+-}
