cbf1c0b50960d1b468404f93e9b24cdebec0aab3
Z Leaf <zach@zincldn.co.uk>
2019-11-27 16:45:20 +0000

    split parser into separate module

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,11 @@ module Main where
 
 import HTTP as HT
 import DB
+import Parser
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C8
-import Data.ByteString.Lazy     as BL
+import Data.ByteString.Lazy as BL
 
 main :: IO ()
 main = do
---
+++ src/Parser.hs
@@ -0,0 +1,30 @@
+module Parser
+    ( parseResponse
+    ) where
+
+import DataTypes
+
+-- JSON modules
+import Data.Aeson as AE
+import Data.Aeson.Types
+--import Data.Aeson.TH
+
+import Data.Text
+import Data.Foldable
+import Control.Monad (join)
+
+parseResponse response = do
+    print $ eitherDecode response >>= parseEither verboseParseMany
+
+{- change/rewrite the below functions - came from https://geekingfrog.com/blog/post/struggles-with-parsing-json-with-aeson -}
+verboseParser :: Value -> Parser (Either String Reporesponse)
+verboseParser v = do
+    case parseEither parseJSON v of
+        Left err -> return . Left $ err ++ " -- Invalid object is: " ++ show v
+        Right parsed -> return $ Right parsed
+
+verboseParseMany :: Value -> Parser [Either String Reporesponse]
+verboseParseMany = withArray "Reporesponse" $ \arr -> do
+    let allParsed = fmap (join . parseEither verboseParser) arr
+    return $ toList allParsed
+    
