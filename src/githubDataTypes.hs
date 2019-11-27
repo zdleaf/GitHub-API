c8c233cc6699d245c34d48f02fef77fe579b6af5
Rizak Hassan <rizak_hassan@yahoo.com>
2019-11-27 15:14:45 +0000

    commiting to pull

--- src/githubDataTypes.hs
+++ src/githubDataTypes.hs
@@ -1,25 +1,13 @@
 module GithubDataTypes where
-import Data.Aeson
-
--- a data type to handle api respones
-data Reporesponse = Reporesponse
-               {
-                  id :: Integer,   -- github api id
-                  languages_url :: String,
-               }
-               deriving (Show, Data, Generic)
 
 
-instance fromJSON Reporesponse where
-                     parseJSON = withObject "Reporesponse" $ \o -> do
-                     id <- o.: "Id"
-                     languages_url <-o.: "Languages_url"
-                     return(Reproresponses id languages_url)
-                  
-                  
-instance ToJSON Repropresponses
-
-
+   -- a data type to handle api respones
+   data Reporesponse = Reporesponse
+                  {
+                     id :: Integer,   -- github api id
+                     languages_url :: String,
+                  }
+                  deriving (Show, Data, Typable)
 
 
 
