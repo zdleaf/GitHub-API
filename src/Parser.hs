69026db931b901d0940a55e19a08dfd0823b827c
Z Leaf <zach@zincldn.co.uk>
2019-11-27 23:11:41 +0000

    general tidy up

--- src/Parser.hs
+++ src/Parser.hs
@@ -12,15 +12,15 @@ import Data.Foldable
 import Control.Monad (join)
 
 parseResponse response = do
-    print $ "successfully decoded JSON"
     let decodedJSON = eitherDecode response >>= parseEither verboseParseMany
+    print $ "successfully decoded JSON"
     return decodedJSON
 
 {- change/rewrite the below functions - came from https://geekingfrog.com/blog/post/struggles-with-parsing-json-with-aeson -}
 verboseParser :: Value -> Parser (Either String Reporesponse)
-verboseParser v = do
-    case parseEither parseJSON v of
-        Left err -> return . Left $ err ++ " -- Invalid object is: " ++ show v
+verboseParser value = do
+    case parseEither parseJSON value of
+        Left err -> return . Left $ err ++ "Invalid object is: " ++ show value
         Right parsed -> return $ Right parsed
 
 verboseParseMany :: Value -> Parser [Either String Reporesponse]
